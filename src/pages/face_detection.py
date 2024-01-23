bf96980359a3856f3f3ca7dc55afaf0bbc07cafe
achrafib1 <achibbuisness512@gmail.com>
2024-01-23 18:17:36 +0100

    Refactor face_detection file and add image
     - Enhance the UI of the detection page
     - Add an image in the images folder under the static folder for the face
       detection UI

--- src/pages/face_detection.py
+++ src/pages/face_detection.py
@@ -25,45 +25,67 @@ def show():
     if page == "Detection Page":
         st.title("Detection Page")
         st.sidebar.subheader("Detection Configuration")
-        with st.container(border=True):
-            col1, col2 = st.columns(2, gap="large")
-            with col1:
-                st.title("Original Image")
-                uploaded_file = st.sidebar.file_uploader(
-                    "Choose an image...", type=["jpg", "jpeg", "png"]
-                )
-                if uploaded_file is not None:
-                    # Convert the file to an image
-                    img_str = base64.b64encode(uploaded_file.read()).decode()
-                    # Create the HTML for the image container
-                    img_container = f"""
-                    <div style="
-                        width: 390px;
-                        height: 300px;
-                        border: 1px solid black;
-                        border-radius: 15px;
-                        box-shadow: 5px 5px 5px grey;
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        background-color: #f0f0f0;
-                        ">
-                        <img src="data:image/png;base64,{img_str}"
-                             alt="Uploaded Image" style="width: 100%; 
-                             height: 100%;
-                             object-fit: cover; 
-                             border-radius: 15px; 
-                             box-shadow: 5px 5px 5px grey;">
-                    </div>
-                    """
-                    components.html(img_container, height=320)
-            with col2:
-                st.title("Detected Face")
-                # components.html(img_container, height=320)
         detection_type = st.sidebar.radio(
             "Choose detection type",
             ["Select Option", "Upload Image", "Real-Time Detection"],
         )
+        with st.container(border=True):
+            col1, col2 = st.columns(2, gap="large")
+            with col1:
+                st.title("Original Image")
+                if detection_type == "Select Option":
+                    choice_container = f"""
+                            <div style="
+                                width: 390px;
+                                height: 300px;
+                                border: 1px solid black;
+                                border-radius: 15px;
+                                box-shadow: 5px 5px 5px grey;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                background-color: #f0f0f0;
+                                ">
+                                <p>Choose detection type in the side bar config section</p>
+                            </div>
+                            """
+                    components.html(choice_container, height=320)
+                if detection_type == "Upload Image":
+                    uploaded_file = st.sidebar.file_uploader(
+                        "Choose an image...", type=["jpg", "jpeg", "png"]
+                    )
+                    if uploaded_file is not None:
+                        # Convert the file to an image
+                        img_str = base64.b64encode(uploaded_file.read()).decode()
+                    else:
+                        img_str = base64.b64encode(
+                            open("static/images/img_uplod.jpg", "rb").read()
+                        ).decode()
+                        # Create the HTML for the image container
+                    img_container = f"""
+                        <div style="
+                            width: 390px;
+                            height: 300px;
+                            border: 1px solid black;
+                            border-radius: 15px;
+                            box-shadow: 5px 5px 5px grey;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            background-color: #f0f0f0;
+                            ">
+                            <img src="data:image/png;base64,{img_str}"
+                                alt="Uploaded Image" style="width: 100%; 
+                                height: 100%;
+                                object-fit: cover; 
+                                border-radius: 15px; 
+                                box-shadow: 5px 5px 5px grey;">
+                        </div>
+                        """
+                    components.html(img_container, height=320)
+            with col2:
+                st.title("Detected Face")
+                # components.html(img_container, height=320)
         if detection_type == "Real-Time Detection":
             st.sidebar.write("Real-Time Detection is selected")
 
