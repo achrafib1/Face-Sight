625ca3eeb12ae6abe495fd2b69d4f17f4c730c1c
achrafib1 <achibbuisness512@gmail.com>
2024-01-24 20:52:46 +0100

    Refactor face detection file
     - Implement real-time webcam detection

--- src/pages/face_detection.py
+++ src/pages/face_detection.py
@@ -3,6 +3,28 @@ import streamlit.components.v1 as components
 import base64
 from PIL import Image, ImageOps
 import numpy as np
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+import av
+
+
+def process_frame(frame):
+    # Make prediction here
+    # result = predict(model, frame)
+
+    # Display the result on the frame
+    # frame = display_result(frame, result)
+
+    return frame
+
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        # Process the image
+        img = process_frame(img)
+
+        return img
 
 
 def show():
@@ -84,49 +106,52 @@ def show():
                         </div>
                         """
                     components.html(img_container, height=320)
-        with col2:
-            st.title("Detected Face")
-            with st.container(border=True):
-                st.markdown(
-                    f"""
-                            <style>
-                            [data-testid="stMarkdown"] {{
-                            background-color: #f0f0f0;
-                            }}
-                            </style>
-                            """,
-                    unsafe_allow_html=True,
-                )
-                uploaded_files = st.file_uploader(
-                    "Choose images for the new container...",
-                    type=["jpg", "jpeg", "png"],
-                    accept_multiple_files=True,
-                )
-                if uploaded_files is not None and len(uploaded_files) > 0:
-                    # Calculate the number of images per row
-                    images_per_row = int(np.ceil(np.sqrt(len(uploaded_files))))
-                    # Create a list to hold the images
-                    images = []
-                    for uploaded_file in uploaded_files:
-                        if uploaded_file is not None:
-                            # Convert the file to an image
-                            image = Image.open(uploaded_file)
-                        else:
-                            image = Image.open("static/images/img_uplod.jpg")
-                        # Resize the image to a fixed size
-                        image = ImageOps.fit(image, (100, 100))
-                        images.append(image)
-                    # Display the images in rows
-                    for i in range(0, len(images), images_per_row):
-                        cols = st.columns(images_per_row)
-                        for j in range(images_per_row):
-                            idx = i + j
-                            if idx < len(images):
-                                cols[j].image(images[idx], use_column_width=True)
+                if detection_type == "Real-Time Detection":
+                    st.sidebar.write("Real-Time Detection is selected")
+                    st.header("Real Time Detection")
+                    webrtc_streamer(
+                        key="example", video_transformer_factory=VideoTransformer
+                    )
 
-                # components.html(img_container, height=320)
-        if detection_type == "Real-Time Detection":
-            st.sidebar.write("Real-Time Detection is selected")
+            with col2:
+                st.title("Detected Face")
+                with st.container(border=True):
+                    st.markdown(
+                        f"""
+                                <style>
+                                [data-testid="stMarkdown"] {{
+                                background-color: #f0f0f0;
+                                }}
+                                </style>
+                                """,
+                        unsafe_allow_html=True,
+                    )
+                    uploaded_files = st.file_uploader(
+                        "Choose images for the new container...",
+                        type=["jpg", "jpeg", "png"],
+                        accept_multiple_files=True,
+                    )
+                    if uploaded_files is not None and len(uploaded_files) > 0:
+                        # Calculate the number of images per row
+                        images_per_row = int(np.ceil(np.sqrt(len(uploaded_files))))
+                        # Create a list to hold the images
+                        images = []
+                        for uploaded_file in uploaded_files:
+                            if uploaded_file is not None:
+                                # Convert the file to an image
+                                image = Image.open(uploaded_file)
+                            else:
+                                image = Image.open("static/images/img_uplod.jpg")
+                            # Resize the image to a fixed size
+                            image = ImageOps.fit(image, (100, 100))
+                            images.append(image)
+                        # Display the images in rows
+                        for i in range(0, len(images), images_per_row):
+                            cols = st.columns(images_per_row)
+                            for j in range(images_per_row):
+                                idx = i + j
+                                if idx < len(images):
+                                    cols[j].image(images[idx], use_column_width=True)
 
     if page == "About":
         st.title("About Page")
