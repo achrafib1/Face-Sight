403dbf469bd1ef31a18c4a337da36d4deae29f9c
achrafib1 <achibbuisness512@gmail.com>
2024-02-18 21:04:52 +0100

    Update predict.py file
     - Enhance the predict function with added descriptions.

--- src/utils/predict.py
+++ src/utils/predict.py
@@ -16,6 +16,28 @@ def predict(
     color="#56ecd5",
     image_replacement=None,
 ):
+    """
+    Predicts the bounding boxes for the detected faces in the image and applies various strategies.
+
+    Parameters:
+    - image: The original image.
+    - model: The object detection model to use.
+    - names: The names of the classes.
+    - faces: A list to store the detected faces.
+    - scale_coords: The function to rescale the coordinates to the original image size.
+    - non_max_suppression: The non-maximum suppression function to apply to the model's predictions.
+    - plot_one_box: The function to draw a bounding box on the image.
+    - strategies: A list of strategies to apply to the image.
+    - background: The desired background color in hexadecimal format.
+    - color: The desired color for the faces in hexadecimal format.
+    - image_replacement: The replacement image as a numpy array.
+
+    Returns:
+    - faces: The list of detected faces.
+    - boxes: The list of bounding boxes for each detected face.
+    - image_with_boxes: The image with the bounding boxes drawn and the strategies applied.
+    """
+
     if image is None:
         print("Error: Could not read image")
         return None, None, None
